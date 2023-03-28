2740a653f220c78ab217672cc1f036081289ad5c
Julia Lachenauer <30265988+Julia-Lachenauer@users.noreply.github.com>
2023-03-28 15:15:34 -0400

    Expanded mongo schema for all parts of a tuit

--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -3,14 +3,16 @@ import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   topic: String,
   username: String,
-  time: String,
-  title: String,
-  image: String,
-  liked: Boolean,
-  replies: Number,
-  retuits: Number,
-  likes: Number,
   handle: String,
-  tuit: String
+  time: String,
+  image: String,
+  title: String,
+  tuit: String,
+  liked: Boolean,
+  likes: Number,
+  disliked: Boolean,
+  dislikes: Number,
+  replies: Number,
+  retuits: Number
 }, {collection: 'tuits'});
 export default schema;
